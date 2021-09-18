c1a46392b2d4f1ffab46ce14c38d4c189974f8cf
changyowen <changyowen@gmail.com>
2021-09-19 05:42:56 +0800

    update

--- Space Minerz/Assets/YowenScript/LaserGunScript.cs
+++ Space Minerz/Assets/YowenScript/LaserGunScript.cs
@@ -6,12 +6,15 @@ public class LaserGunScript : MonoBehaviour
 {
     [Header("Reference")]
     public GameObject laserBeam_obj;
+    public GameObject miningLaserBeam_obj;
     public Transform gunPoint_transform;
 
     [Header("Value Data")]
     public float gunRefreshTime = 0.5f;
+    public float miningGunRefreshTime = 0.5f;
 
     float gunRefresh = 0;
+    float miningGunRefresh = 0;
 
     private void Update()
     {
@@ -22,6 +25,11 @@ public class LaserGunScript : MonoBehaviour
             if(gunRefresh <= 0)
                 ShootRay();
         }
+        else if(Input.GetMouseButton(1))
+        {
+            if (miningGunRefresh <= 0)
+                ShootMiningRay();
+        }
     }
 
     void GunRefresh()
@@ -29,6 +37,10 @@ public class LaserGunScript : MonoBehaviour
         if (gunRefresh > 0)
         {
             gunRefresh -= Time.deltaTime;
+        }
+        if(miningGunRefresh > 0)
+        {
+            miningGunRefresh -= Time.deltaTime;
         }
     }
 
@@ -43,5 +55,18 @@ public class LaserGunScript : MonoBehaviour
         }
 
         gunRefresh += gunRefreshTime;
+    }
+
+    void ShootMiningRay()
+    {
+        GameObject newMiningLaserBeam = Instantiate(miningLaserBeam_obj, gunPoint_transform.position, transform.rotation) as GameObject;
+        ShotBehavior shotBehavior = newMiningLaserBeam.GetComponent<ShotBehavior>();
+        MainMovement mainMovement = GetComponent<MainMovement>();
+        if (shotBehavior != null && mainMovement != null)
+        {
+            shotBehavior.speed = mainMovement.playerSpeed * 4;
+        }
+
+        miningGunRefresh += miningGunRefreshTime;
     }
 }
