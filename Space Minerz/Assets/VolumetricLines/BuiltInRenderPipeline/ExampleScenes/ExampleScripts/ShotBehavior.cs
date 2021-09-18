84bf7dc9ffa23fdea7b316661d53821fbf186d94
changyowen <changyowen@gmail.com>
2021-09-18 13:20:56 +0800

    Hit effect

--- Space Minerz/Assets/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
+++ Space Minerz/Assets/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class ShotBehavior : MonoBehaviour {
 
+	[Header("Reference")]
+	public GameObject hitEffect_obj;
+
+	[Header("Data Value")]
 	public float speed = 20f;
 
 	// Use this for initialization
@@ -11,8 +15,14 @@ public class ShotBehavior : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position += /*transform.rotation * */transform.forward * Time.deltaTime * speed;
-	
+	void Update () 
+	{
+		transform.position += transform.forward * Time.deltaTime * speed;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		GameObject newHitEffect = Instantiate(hitEffect_obj, collision.contacts[0].point, Quaternion.identity) as GameObject;
+		Destroy(this.gameObject);
 	}
 }
